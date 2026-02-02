e7ff7dd1f5a23a3aeba1d5f11446227dd95c6a40
Shop4Me <shop4me.market@gmail.com>
2026-02-02 12:32:50 +0000

    Changes

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -135,8 +135,6 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* Bottom Gradient */}
-      <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background to-transparent z-10" />
     </section>
   );
 };
