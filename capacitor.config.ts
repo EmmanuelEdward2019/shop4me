4ff1c58a06e3b28e61ce3334f01faf43961720b8
Shop4Me <shop4me.market@gmail.com>
2026-02-27 08:51:25 +0000

    Add Capacitor config for web app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+{
+  "appId": "app.lovable.4126a2ed92934454aef38c365523a25a",
+  "appName": "shop4me",
+  "webDir": "dist",
+  "server": {
+    "url": "https://4126a2ed-9293-4454-aef3-8c365523a25a.lovableproject.com?forceHideBadge=true",
+    "cleartext": true
+  }
+}
